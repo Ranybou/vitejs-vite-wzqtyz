a62dc4e83338f6dd554cca595bc52317b32dad08
Ranybou <144000639+Ranybou@users.noreply.github.com>
2023-10-03 11:03:39 +0200

    1 file modified

--- data.js
+++ data.js
@@ -4,62 +4,62 @@ export const data = [
     titre: 'Nouvelles découvertes en astronomie',
     contenu:
       'Des astronomes du monde entier ont fait des découvertes fascinantes sur de nouvelles exoplanètes. Ces exoplanètes pourraient potentiellement abriter la vie.',
-    image: 'img/1.png',
+    image: '/1.png',
   },
   {
     id: 2,
     titre: 'Avancées dans la médecine',
     contenu:
       "Des chercheurs ont récemment mis au point un traitement révolutionnaire pour lutter contre certaines maladies graves. Cette avancée promet d'améliorer la vie de nombreuses personnes.",
-    image: 'img/2.png',
+    image: '/2.png',
   },
   {
     id: 3,
     titre: 'Le changement climatique en chiffres',
     contenu:
       "Une nouvelle étude met en lumière l'impact croissant du changement climatique. Les chiffres sont alarmants, et des actions immédiates sont nécessaires pour atténuer ses effets.",
-    image: 'img/3.png',
+    image: '/3.png',
   },
   {
     id: 4,
     titre: 'Découverte archéologique majeure',
     contenu:
       "Des archéologues ont récemment mis au jour une ancienne cité perdue depuis des siècles. Cette découverte pourrait révolutionner notre compréhension de l'histoire ancienne.",
-    image: 'img/4.png',
+    image: '/4.png',
   },
   {
     id: 5,
     titre: 'Technologie de pointe',
     contenu:
       'Les dernières avancées technologiques ouvrent de nouvelles perspectives passionnantes. Des entreprises du monde entier travaillent sur des innovations qui pourraient changer notre façon de vivre.',
-    image: 'img/5.png',
+    image: '/5.png',
   },
   {
     id: 6,
     titre: 'Exploration spatiale',
     contenu:
       "Les agences spatiales préparent de nouvelles missions d'exploration spatiale pour découvrir les secrets de l'univers. Les astronautes espèrent faire des découvertes exceptionnelles dans l'espace lointain.",
-    image: 'img/6.png',
+    image: '/6.png',
   },
   {
     id: 7,
     titre: 'Art et culture contemporains',
     contenu:
       "Le monde de l'art contemporain est en constante évolution. De nouveaux artistes émergent avec des œuvres innovantes qui défient les conventions traditionnelles.",
-    image: 'img/7.png',
+    image: '/7.png',
   },
   {
     id: 8,
     titre: 'Voyages exotiques',
     contenu:
       "Les voyageurs avides d'aventure explorent des destinations exotiques aux paysages à couper le souffle. Découvrez les endroits les plus étonnants à visiter dans le monde.",
-    image: 'img/8.png',
+    image: '/8.png',
   },
   {
     id: 9,
     titre: 'Innovations en énergie propre',
     contenu:
       "Les scientifiques travaillent sur de nouvelles technologies d'énergie propre pour lutter contre le changement climatique. Ces innovations pourraient transformer la façon dont nous produisons et utilisons l'énergie.",
-    image: 'img/9.png',
+    image: '/9.png',
   },
 ];
